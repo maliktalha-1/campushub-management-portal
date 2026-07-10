9fa2f86e022a6de3dad378f2cf4c5bfbba4b467b
maliktalha-1 <talhaazam337@gmail.com>
2026-07-10 05:01:06 -0700

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,12 +6,17 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-// app.enableCors({
-//   origin: "*",
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true,
-// });
+  // Enable CORS
+  app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "https://campushub-management-portal-qoub1oocq-maliktalha-1s-projects.vercel.app",
+      "https://campushub-management-portal-1qug4bejo-maliktalha-1s-projects.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -46,9 +51,9 @@ async function bootstrap() {
 
   const port = process.env.PORT || 5000;
 
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 
-  console.log(`🚀 Backend running on http://localhost:${port}`);
+  console.log(`🚀 Backend running on port ${port}`);
   console.log(`📚 Swagger Docs: http://localhost:${port}/api`);
 }
 
