74e050585d2c5bc59e9bbf1f3649e716dab0d115
maliktalha-1 <talhaazam337@gmail.com>
2026-07-10 04:45:31 -0700

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,10 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 app.enableCors({
-  origin: [
-    "http://localhost:3000",
-    "https://campushub-management-portal-1qug4bejo-maliktalha-1s-projects.vercel.app",
-  ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
