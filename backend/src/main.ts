771e34b5130734d44bb07bff75d2a955da0f9594
maliktalha-1 <talhaazam337@gmail.com>
2026-07-10 04:53:49 -0700

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,12 +6,12 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-app.enableCors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-});
+// app.enableCors({
+//   origin: "*",
+//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+// });
 
   app.useGlobalPipes(
     new ValidationPipe({
